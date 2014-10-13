98f14b8734c5040ed4500e5dfda0614d4a4a1dec
Philip Nguyen <supertaru@gmail.com>
2014-10-13 16:09:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaperclipExample::Application.config.secret_key_base = 'c6a80546eef0f6c7853a04df8bb76bb4f6e72d403de13caa002587573c9e7a6fcd598d34c8440f812d7efa725c5c9f4a25c82f50d9cd2bc39c3feb09e11b3b3d'
